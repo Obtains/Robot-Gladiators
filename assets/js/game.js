beb53cee6094f82075880d5bc9beaebec344c825
Failsafe <hawkhilt@gmail.com>
2022-02-10 13:38:38 -0500

    added fight loop

--- assets/js/game.js
+++ assets/js/game.js
@@ -9,8 +9,8 @@ var enemyHealth = 50;
 var enemyAttack = 12;
 
 var fight = function(enemyName) {
-    // Alert players that they are starting the round
-    window.alert("Welcome to Robot Gladiators!");
+   // repeat and excute as long as the enemy-robot is alive
+   while(enemyHealth > 0) {
 
     var promptFight = window.prompt("Would you like to FIGHT or SKIP this battle? Enter 'FIGHT' or 'SKIP' to choose.");
 
@@ -45,7 +45,7 @@ var fight = function(enemyName) {
         window.alert(playerName + " still has " + playerHealth + " health left ");
     }
     // if player choses to skip
-}   else if (promptFight === "skip" || promptFight === "SKIP") {
+    }else if (promptFight === "skip" || promptFight === "SKIP") {
     // confirm player wants to skip
     var confirmSkip = window.confirm("Are you sure you'd like to quit?");
     
@@ -62,10 +62,14 @@ var fight = function(enemyName) {
 
  }  else {
     window.alert("You need to choose a valid option. Try again!");
-}
 
+
+}
+}
 };
 
 for(var i = 0; i < enemyNames.length; i++) {
-    fight(enemyNames[i]);
+    var pickedEnemyName = enemyNames[i];
+    enemyHealth = 50;
+    fight(pickedEnemyName);
 }
